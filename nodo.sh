ba2b598d7bb3f9856fbad01df00d3c63d2d9cdb7
José Rafael <carmonajose148@gmail.com>
2020-12-05 16:48:00 -0600

    se arreglo un bug de keepalived

--- nodo.sh
+++ nodo.sh
@@ -66,6 +66,23 @@ function conectarse_swarm(){
     echo '   OK'
 }
 
+
+function install_keepalived(){
+         docker run -d --name keepalived --restart=always \
+              --cap-add=NET_ADMIN --cap-add=NET_BROADCAST --cap-add=NET_RAW --net=host \
+              -e KEEPALIVED_INTERFACE=$4 \
+              -e KEEPALIVED_UNICAST_PEERS="#PYTHON2BASH:[$1,$2]" \
+              -e KEEPALIVED_VIRTUAL_IPS=$3 \
+              -e KEEPALIVED_PRIORITY=100 \
+              osixia/keepalived
+}
+
+function keepalived(){
+        IP_VIRTUAL=$(ssh root@$1 cat /root/.ip_virtual)
+        IP_NODO=$(ssh root@$1 cat /root/.ip_nodo)
+        install_keepalived $1 $IP_NODO $IP_VIRTUAL $2
+}
+
 ip_master=$1
 punto_montaje=$2
 interface=$3
@@ -88,9 +105,8 @@ echo 'Iniciando la instalacion de ceph..'
 chmod +x ceph/install_ceph.sh
 chmod -R +x ceph/
 cd ceph/ && bash ./install_ceph.sh "$ip_master" "$punto_montaje"
-if [ $nodo -eq 2 ]; then
+if [[ $nodo -eq 2 ]]; then
     echo '-->Instalando keepalived'
-    chmod -R +x keepalived/
-    cd keepalived/ && bash ./install_keepalived.sh "$ip_master" "$interface"
+    keepalived "$ip_master" "$interface"
     echo "-->listo"
 fi
